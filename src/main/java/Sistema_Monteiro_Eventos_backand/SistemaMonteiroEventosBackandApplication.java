f19e7aba2d5aca9b70ae19d5d81d960a70eb6db6
João Vítor Ferreira <jv96516609@gmail.com>
2026-08-05 18:50:30 -0300

    Projeto Spring Boot inicial - Sistema Monteiro Eventos backend

---
+++ src/main/java/Sistema_Monteiro_Eventos_backand/SistemaMonteiroEventosBackandApplication.java
@@ -0,0 +1,13 @@
+package Sistema_Monteiro_Eventos_backand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMonteiroEventosBackandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMonteiroEventosBackandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sistema_Monteiro_Eventos_backand/SistemaMonteiroEventosBackandApplicationTests.java
@@ -0,0 +1,13 @@
+package Sistema_Monteiro_Eventos_backand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMonteiroEventosBackandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
